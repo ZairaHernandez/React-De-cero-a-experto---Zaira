78f6d00528cab6fabec9f713c201277d4447c353
ZairaHernandez <86864292+ZairaHernandez@users.noreply.github.com>
2022-11-07 22:10:24 -0600

    Seccion 01, 02 y inicios 03

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+//Variables y constantes
+
+const nombre = "Zaira";
+const apellido = "Hernandez";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+// var No se debe usar
+if (true) {
+    const nombre = "peter";
+    console.log(nombre);
+}
+
+console.log(valorDado);
