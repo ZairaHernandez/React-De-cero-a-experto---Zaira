754d3c2d9e4c18535fac6df4a14b5bb55c74cfad
ZairaHernandez <86864292+ZairaHernandez@users.noreply.github.com>
2022-11-09 20:13:27 -0600

    Finalizacion modulo 3

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,7 +1,7 @@
 // import { heroes } from './data/heroes'
 // import {heroes} from './data/heroes' 
 //const { heroes } = require("./data/heroes"); 
-import heroes, {owners} from './data/heroes'
+import heroes, {owners} from '../data/heroes'
 
 //console.log(heroes);
 
