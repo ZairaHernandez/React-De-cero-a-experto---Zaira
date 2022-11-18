2759cb00fa426e0f1591f2c09ab4d2cb13b70bbb
ZairaHernandez <86864292+ZairaHernandez@users.noreply.github.com>
2022-11-18 00:42:25 -0600

    GifExpertApp terminado 18/11/22

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,8 +1,19 @@
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
 export const GifGrid = ({ category }) => {
+  const { images, isLoading } = useFetchGifs(category);
+
   return (
     <>
       <h3>{category}</h3>
-      <p>Hola mundo</p>
+      {isLoading && <h2>Cargando...</h2>}
+
+      <div className="card-grid">
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
+      </div>
     </>
   );
 };
