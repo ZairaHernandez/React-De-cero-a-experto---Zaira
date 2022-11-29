7b6f0f7414f1c95652bb94bf6dff5444901d2088
ZairaHernandez <86864292+ZairaHernandez@users.noreply.github.com>
2022-11-28 21:48:50 -0600

    Seccion 11

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 //import { Padre } from './07-tarea-memo/Padre';
 //import { CallbackHook } from './06-memos/CallBackHook';
 //import { MemoHook } from './06-memos/MemoHook';
@@ -15,10 +17,16 @@ import ReactDOM from 'react-dom/client';
 //import { HooksApp } from './HooksApp';
 import './index.css';
 //import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
+//import { TodoApp } from './08-useReducer/TodoApp';
+
+import { MainApp } from './09-useContext/MainApp'
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TodoApp/>
-  </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 )
